d98425a35bbe1ce49a150b83d4008185797829e7
Muhammad Waqar <mw6069605@gmail.com>
2022-09-06 15:48:46 +0500

    datatypes comments

--- part3-datatypes/data-types.js
+++ part3-datatypes/data-types.js
@@ -1,6 +1,26 @@
-
+//number constant cannot be updated after words
 const _name=33;
-console.log(_name);
+;
+//let can be updated after words
+let _word='waqar';
 
-_name='waqar';
-console.log(_name);
+
+/*var can also be use but not recommende can cause alot of bugs and can be accessed globally*/
+
+var _new;
+_new=true;
+
+console.log(typeof _word)
+console.log(typeof _new)
+console.log(typeof _name)
+
+
+//dyname typing 
+let jsisfun=true;
+
+console.log(jsisfun)
+
+jsisfun=33;
+console.log(jsisfun)
+jsisfun='hello-mate'
+console.log(jsisfun)
