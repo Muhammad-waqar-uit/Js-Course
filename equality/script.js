15ee6437b3a88696e2f15e52fa0db3db794f8071
Muhammad-waqar-uit <muwaqar@students.uit.edu>
2022-09-10 11:48:58 +0500

    equality operators

---
+++ equality/script.js
@@ -0,0 +1,10 @@
+//equality operators
+//strick equality ===
+//normal equal that does tyoe coersion ==
+const age='18';
+
+if (age===18){
+    console.log('old enough');
+}else{
+    console.log('nope');
+}
