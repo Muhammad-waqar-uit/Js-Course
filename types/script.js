2cb479df707399820bf265b31bfce754026f6984
Muhammad-waqar-uit <muwaqar@students.uit.edu>
2022-09-10 11:32:47 +0500

    coercion

---
+++ types/script.js
@@ -0,0 +1,14 @@
+const input='1991';
+
+console.log(Number(input)+18);
+
+console.log(String(19));
+
+//type coercion
+console.log(`I am `+22+' years old');
+
+console.log('23'-10-'3');
+
+console.log('13'*'10');
+
+console.log('10'/'2');
