2117ee9f278042338a79fa12600f214409809a22
Muhammad-waqar-uit <muwaqar@students.uit.edu>
2022-09-10 12:39:18 +0500

    swicth statement

---
+++ switch/script.js
@@ -0,0 +1,12 @@
+const day='nice';
+
+switch(day){
+    case 'monday':
+        console.log('do more code!');
+        break;
+    case 'tuesday':
+        console.log('just chill');
+        break;
+    default:
+        console.log('u dumb Person! put the day only')
+}
