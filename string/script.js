3e371f6dd527468b3a6d3286dfa721e0c5455987
Muhammad-waqar-uit <muwaqar@students.uit.edu>
2022-09-10 10:46:03 +0500

    string litterals

---
+++ string/script.js
@@ -0,0 +1,18 @@
+const firstname='waqar';
+const job='NoJob';
+const Birthyear=2000;
+
+const waqar=' I am ' + firstname +' , a ' + (Birthyear-2020)+" year old.";
+
+console.log(waqar);
+
+const waqarnew=`I'm ${firstname}, a ${2022-Birthyear} year old.`;
+
+console.log(waqarnew);
+
+//multi line string
+
+console.log('string with \n new line')
+//string litterals multi line string
+console.log(`string 
+multi line`)
