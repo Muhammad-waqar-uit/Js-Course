7d2cf047d7cc29fe3ab194439d6b89afbe1f7a02
Muhammad-waqar-uit <muwaqar@students.uit.edu>
2022-09-16 18:02:14 +0500

    challenge-2

---
+++ code challenge/challenge2.js
@@ -0,0 +1,8 @@
+const calctip=(bill)=>{
+    return bill>=50 && bill<=300 ? bill*0.15 : bill * 0.2; 
+}
+
+const bills=[125,555,44];
+
+const tips=[calctip(bills[0]),calctip(bills[1]),calctip(bills[2])];
+console.log(bills,tips);
